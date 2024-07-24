517b264f702a839b63b532c4cb5d94918b06ef14
Hemangi Patil <hemangipatil65@gmail.com>
2024-07-24 19:11:08 +0530

    last commit

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 require("./db/conn");
+const path=require("path");
 const cookieParser=require("cookie-parser");
 
 
@@ -11,12 +12,17 @@ const Products=require("./models/productsSchema");
 const DefaultData=require("./defaultdata");
 const cors=require("cors");
 const router=require("./routes/router");
+const { dirname } = require("path/posix");
 
 app.use(express.json());
 app.use(cookieParser(""));
 app.use(cors());
 app.use(router);
 
+app.get("/",(req,res)=>{
+    app.use(express.static(path.resolve(__dirname,"client","build")));
+    res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+})
 
 const port=8005;
 
